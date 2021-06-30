834a3d716b2e0732b1c68420d009610490d78673
Sourav R <developer.srv1@gmail.com>
2021-06-30 15:08:46 +0530

    getter local scopes

--- src/Models/ModelAction.php
+++ src/Models/ModelAction.php
@@ -5,11 +5,12 @@ namespace Devsrv\ScheduledAction\Models;
 use Illuminate\Database\Eloquent\Model;
 use Devsrv\ScheduledAction\Enums\Status;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
 
 class ModelAction extends Model
 {
     use HasFactory;
-
+    
     protected $table = 'model_actions';
 
     protected $guarded = [];
@@ -21,6 +22,10 @@ class ModelAction extends Model
         'finished_at' => 'datetime'
     ];
 
+    private static function today() : string {
+        return Carbon::now()->toDateString();
+    }
+
     public function actionable()
     {
         return $this->morphTo();
@@ -29,5 +34,43 @@ class ModelAction extends Model
     public function scopeFinished($query)
     {
         return $query->where('status', Status::FINISHED);
+    }
+
+    public function scopePending($query)
+    {
+        return $query->where('status', Status::PENDING);
+    }
+
+    public function scopeCancelled($query)
+    {
+        return $query->where('status', Status::CANCELLED);
+    }
+
+    public function scopeDispatched($query)
+    {
+        return $query->where('status', Status::DISPATCHED);
+    }
+
+    public function scopeToActToday($query)
+    {
+        return $query->whereDate('act_at', self::today());
+    }
+
+    public function scopeToActBetween($query, $start, $end)
+    {
+        return $query->whereBetween('act_at', [$start, $end]);
+    }
+
+    public function scopeWhereProperty($query, $prop, $value)
+    {
+        return $query->whereJsonContains('properties->' . $prop, $value);
+    }
+
+    public function scopeWhereProperties($query, array $properties)
+    {
+        collect($properties)
+        ->each(fn($value, $key) => $query->whereJsonContains('properties->' . $key, $value));
+
+        return $query;
     }
 }
