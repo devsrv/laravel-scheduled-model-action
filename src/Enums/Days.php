e2afeb6db69dfa3a734887adfbcfbda820c587be
Sourav R <developer.srv1@gmail.com>
2021-07-05 15:06:32 +0530

    days list as enum

---
+++ src/Enums/Days.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Devsrv\ScheduledAction\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class Days extends Enum
+{
+    const SUNDAY = 'SUNDAY';
+    const MONDAY = 'MONDAY';
+    const TUESDAY = 'TUESDAY';
+    const WEDNESDAY = 'WEDNESDAY';
+    const THURSDAY = 'THURSDAY';
+    const FRIDAY = 'FRIDAY';
+    const SATURDAY = 'SATURDAY';
+}
