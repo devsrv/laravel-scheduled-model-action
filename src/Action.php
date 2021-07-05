6651de9d2adf58abf98cfcf36e667d1d3d0d8296
Sourav R <developer.srv1@gmail.com>
2021-07-05 19:34:28 +0530

    main action class

---
+++ src/Action.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Devsrv\ScheduledAction;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Devsrv\ScheduledAction\Models\ModelAction;
+
+class Action
+{
+    public function emit()
+    {
+        
+    }
+
+    protected function getPendingSchedules($date = null, $limit = 10, $orderBy = 'asc') : Collection {
+        if($orderBy !== 'asc') $orderBy = 'desc';
+
+        $date = $date ?? Carbon::now();
+        $day = strtoupper($date->englishDayOfWeek);
+
+        return ModelAction::pending()
+        ->where(function($query) use($date) {
+            $query
+            ->whereDate('act_on', $date);
+        })
+        ->when($day, function ($query, $day) {
+            return $query
+                ->orWhere(function($query) use ($day) {
+                    $query->whereHas('recurringDays', function (Builder $query) use ($day) {
+                        $query->where('day', $day);
+                    });
+                });
+        })
+        ->orderByRaw('TIME(`act_at`) '. $orderBy)
+        ->take($limit)
+        ->get();
+    }
+
+    /**
+     * the list of scheduled pending actions to run today
+     * @param int $take optional how many results to show
+     * @param string $orderBy optional order by time when to run the process 'asc' | 'desc'
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function needsToRunToday($take = 10, $orderBy = 'asc') : Collection {
+        return $this->getPendingSchedules(Carbon::now(), $take, $orderBy);
+    }
+
+    /**
+     * the list of scheduled pending actions to run on a given date
+     * @param Carbon $date target date
+     * @param int $take optional how many results to show
+     * @param string $orderBy optional order by time when to run the process 'asc' | 'desc'
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function needsToRunOn(Carbon $date, $take = 10, $orderBy = 'asc') : Collection {
+        return $this->getPendingSchedules($date, $take, $orderBy);
+    }
+}
