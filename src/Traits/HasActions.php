69a4d3582f6674bea7ec6246f755f451c6da3f7f
Sourav R <developer.srv1@gmail.com>
2021-07-07 00:08:04 +0530

    delete any scheduled action when main model is deleted

--- src/Traits/HasActions.php
+++ src/Traits/HasActions.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 trait HasActions
 {
+    protected static function booted()
+    {
+        parent::booted();
+
+        static::deleting(function ($model) {
+            $model->scheduledActions()->delete();
+        });
+    }
+
     public function scheduledActions(): MorphMany
     {
         return $this->morphMany(ModelAction::class, 'actionable');
